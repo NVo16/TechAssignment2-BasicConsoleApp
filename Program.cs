0cd98431330c7491a71d24ddc107a1c7673e219e
Ngoc Vo <ngocvo@usf.edu>
2022-01-28 15:20:55 -0500

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,13 @@
+﻿// Programmer: Ngoc Vo
+// Project Name: Tech Assignment #2 - Basic Console App
+// Due Date: 1/28/2022
+// Description: This is the first assignment to confirm the connection to the GitHub account
+
+// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello! Welcome to my first assignment!");
+Console.WriteLine("My name is Ngoc Vo.");
+Console.WriteLine("I know it is not easy to learn programming but I really love it.");
+Console.WriteLine("I do not have much experiences in this field but I look forward to learning more from you.");
+Console.WriteLine("Thank you for reading my message!");
+
+
