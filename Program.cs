38a49e363f3b40371f2da7a4627dd9aad349d815
Ngoc Vo <ngocvo@usf.edu>
2022-01-28 17:42:15 -0500

    Commit changes

--- Program.cs
+++ Program.cs
@@ -6,6 +6,6 @@
 // See https://aka.ms/new-console-template for more information
 Console.WriteLine("Hello! Welcome to my first assignment!");
 Console.WriteLine("My name is Ngoc Vo.");
-Console.WriteLine("I know it is not easy to learn programming but I really love it.");
-Console.WriteLine("I do not have much experiences in this field but I look forward to learning more from you.");
+Console.WriteLine("I know it is not easy to learn how to program, but I really love it.");
+Console.WriteLine("I do not have much experience in this field, but I look forward to learning more from you.");
 Console.WriteLine("Thank you for reading my message!");
